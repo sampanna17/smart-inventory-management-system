e4a54d3bd6ba5a103c82f47dc880a101d608c543
sampanna17 <sampannapiya13@gmail.com>
2026-07-09 14:35:02 +0545

    Cloudinary Delete Iamge Update

--- src/main/java/com/smartinventorysystem/modules/productimage/service/ProductImageServiceImpl.java
+++ src/main/java/com/smartinventorysystem/modules/productimage/service/ProductImageServiceImpl.java
@@ -1,105 +1,88 @@
-package com.smartinventorysystem.modules.productimage.service;
+    package com.smartinventorysystem.modules.productimage.service;
 
-import com.smartinventorysystem.common.service.CloudinaryService;
-import com.smartinventorysystem.exceptions.ResourceNotFoundException;
-import com.smartinventorysystem.modules.product.entity.Product;
-import com.smartinventorysystem.modules.product.repository.ProductRepository;
-import com.smartinventorysystem.modules.productimage.dto.Response.ProductImageResponse;
-import com.smartinventorysystem.modules.productimage.entity.ProductImage;
-import com.smartinventorysystem.modules.productimage.mapper.ProductImageMapper;
-import com.smartinventorysystem.modules.productimage.repository.ProductImageRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+    import com.smartinventorysystem.common.service.CloudinaryService;
+    import com.smartinventorysystem.exceptions.ResourceNotFoundException;
+    import com.smartinventorysystem.modules.product.entity.Product;
+    import com.smartinventorysystem.modules.product.repository.ProductRepository;
+    import com.smartinventorysystem.modules.productimage.dto.Response.ProductImageResponse;
+    import com.smartinventorysystem.modules.productimage.entity.ProductImage;
+    import com.smartinventorysystem.modules.productimage.mapper.ProductImageMapper;
+    import com.smartinventorysystem.modules.productimage.repository.ProductImageRepository;
+    import jakarta.transaction.Transactional;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.stereotype.Service;
+    import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.Map;
+    import java.util.List;
+    import java.util.Map;
 
-@Service
-@RequiredArgsConstructor
-public class ProductImageServiceImpl implements ProductImageService {
+    @Service
+    @RequiredArgsConstructor
+    public class ProductImageServiceImpl implements ProductImageService {
 
-    private final ProductRepository productRepository;
-    private final ProductImageRepository imageRepository;
-    private final CloudinaryService cloudinaryService;
-    private final ProductImageMapper mapper;
+        private final ProductRepository productRepository;
+        private final ProductImageRepository imageRepository;
+        private final CloudinaryService cloudinaryService;
+        private final ProductImageMapper mapper;
 
-    @Override
-    public ProductImageResponse uploadImage(
-            Integer productId,
-            MultipartFile file
-    ) {
+        @Override
+        public ProductImageResponse uploadImage(Integer productId, MultipartFile file) {
 
-        Product product = productRepository.findById(productId)
-                .orElseThrow(
-                        () -> new ResourceNotFoundException(
-                                "Product not found"
-                        )
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(
+                            () -> new ResourceNotFoundException(
+                                    "Product not found"
+                            )
+                    );
+
+            Map<String, Object> uploadResult = cloudinaryService.uploadImage(file);
+
+            ProductImage image = new ProductImage();
+
+            image.setProduct(product);
+            image.setImageURL(uploadResult.get("secure_url").toString());
+            image.setPublicId(uploadResult.get("public_id").toString());
+
+            return mapper.toResponse(imageRepository.save(image));
+        }
+
+        @Override
+        public List<ProductImageResponse> getImages(Integer productId) {
+
+            if(!productRepository.existsById(productId)){
+                throw new ResourceNotFoundException(
+                        "Product not found"
                 );
-
-        Map<String, Object> uploadResult =
-                cloudinaryService.uploadImage(file);
-
-        ProductImage image = new ProductImage();
-
-        image.setProduct(product);
-        image.setImageURL(
-                uploadResult.get("secure_url").toString()
-        );
-        image.setPublicId(
-                uploadResult.get("public_id").toString()
-        );
-
-        return mapper.toResponse(
-                imageRepository.save(image)
-        );
-
-    }
-
-    @Override
-    public List<ProductImageResponse> getImages(
-            Integer productId
-    ) {
-
-        if(!productRepository.existsById(productId)){
-
-            throw new ResourceNotFoundException(
-                    "Product not found"
-            );
-        }
-        return mapper.toResponseList(
-                imageRepository.findByProductProductId(productId)
-        );
-    }
-
-    @Transactional
-    @Override
-    public void deleteImage(Integer productId, Integer imageId) {
-
-        ProductImage image =
-                imageRepository.findById(imageId)
-                        .orElseThrow(
-                                () -> new ResourceNotFoundException(
-                                        "Image not found"
-                                )
-                        );
-
-        if(!image.getProduct()
-                .getProductId()
-                .equals(productId)){
-
-            throw new RuntimeException(
-                    "Image does not belong to product"
+            }
+            return mapper.toResponseList(
+                    imageRepository.findByProductProductId(productId)
             );
         }
 
-        // Delete from Cloudinary
-        cloudinaryService.deleteImage(
-                image.getPublicId()
-        );
+        @Transactional
+        @Override
+        public void deleteImage(Integer productId, Integer imageId) {
 
-        imageRepository.delete(image);
+            ProductImage image =
+                    imageRepository.findById(imageId)
+                            .orElseThrow(
+                                    () -> new ResourceNotFoundException(
+                                            "Image not found"
+                                    )
+                            );
 
+            if(!image.getProduct()
+                    .getProductId()
+                    .equals(productId)){
+
+                throw new RuntimeException("Image does not belong to product");
+            }
+
+            // Delete From Cloudinary
+            cloudinaryService.deleteImage(image.getPublicId());
+
+            //Delete From Database
+            imageRepository.delete(image);
+
+        }
     }
-}
